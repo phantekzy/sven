413b993a383d5937f97405b72da9c2fde46614e0
phantekzy <mainilotfi@gmail.com>
2026-01-05 17:08:57 +0100

    MongoDB configuration

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -3,6 +3,12 @@ import mongoose from "mongoose";
 /* Connect to mongo DB */
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-  } catch (error) {}
+    /* Connection */
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected : ${conn.connection.host}`);
+  } catch (error) {
+    console.log("Can not connect to MongoDB ", error);
+    /* in a failure case  */
+    process.exit(1);
+  }
 };
