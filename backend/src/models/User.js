289e22cb08bce9c1987c20736b86087a9e14b790
phantekzy <mainilotfi@gmail.com>
2026-01-05 20:51:06 +0100

    created User schema with some fields and timestamps , and added a salt and hashing and a condition to not salt and hash none modified passwords

---
+++ backend/src/models/User.js
@@ -0,0 +1,70 @@
+/* Import section */
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+/* Blueprint */
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    bio: {
+      type: String,
+      default: "",
+    },
+    profilePic: {
+      type: String,
+      default: "",
+    },
+    nativeLanguage: {
+      type: String,
+      default: "",
+    },
+    learningLanguage: {
+      type: String,
+      default: "",
+    },
+    location: {
+      type: String,
+      default: "",
+    },
+    isOnboarded: {
+      type: Boolean,
+      default: false,
+    },
+
+    friends: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  { timestamps: true },
+);
+/* Pre hook salt and hash the users password*/
+userSchema.pre("save", async function (next) {
+  /* Modified condition */
+  if (!this.isModified("password")) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+/* User collection  */
+const User = mongoose.model("User", userSchema);
+/* Export section */
+export default User;
