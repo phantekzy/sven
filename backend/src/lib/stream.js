bd704a8bb4890aa002bf1b7346a2bd12aab0c557
phantekzy <mainilotfi@gmail.com>
2026-01-07 19:14:04 +0100

    creating or updating Stream users

---
+++ backend/src/lib/stream.js
@@ -0,0 +1,23 @@
+/* Import section */
+import { StreamChat } from "stream-chat";
+import "dotenv/config";
+
+/* STREAM API KEYS */
+const apiKey = process.env.STREAM_API_KEY;
+const apiSecret = process.env.STREAM_API_SECRET;
+
+/* Missing API condition */
+if (!apiKey || !apiSecret) {
+  console.error("Stream api key or Stream api secret is missing");
+}
+
+/* Stream client section */
+const streamClient = StreamChat.getInstance(apiKey, apiSecret);
+/* Create or Update Stream users function  */
+export const upsertStreamUser = async (userData) => {
+  try {
+    await streamClient.upsertUser([userData]);
+  } catch (error) {
+    console.error(error);
+  }
+};
