d2b6fa8af3d39b07e6a34e1e5bbca8c61cde410e
phantekzy <mainilotfi@gmail.com>
2026-01-10 23:27:40 +0100

    feat: implement onboarding logic with validation and stream sync

--- backend/src/controlers/auth.controler.js
+++ backend/src/controlers/auth.controler.js
@@ -133,4 +133,59 @@ export function Logout(req, res) {
 }
 
 /* ---- ONBOARD SECTION ----- */
-export async function onboard(req, res) {}
+export async function onboard(req, res) {
+  try {
+    const userId = req.user._id;
+    const { fullName, bio, nativeLanguage, learningLanguage, location } =
+      req.body;
+    if (
+      !fullName ||
+      !bio ||
+      !nativeLanguage ||
+      !learningLanguage ||
+      !location
+    ) {
+      return res.status(400).json({
+        message: "All fields are required",
+        missingFields: [
+          !fullName && "fullName",
+          !bio && "bio",
+          !nativeLanguage && "nativeLanguage",
+          !learningLanguage && "learningLanguage",
+          !location && "location",
+        ].filter(Boolean),
+      });
+    }
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      {
+        ...req.body,
+        isOnboarded: true,
+      },
+      { new: true },
+    );
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    // Update Stream user info
+    try {
+      await upsertStreamUser({
+        id: updatedUser._id.toString(),
+        name: updatedUser.fullName,
+        image: updatedUser.profilePic || "",
+      });
+      console.log(
+        `Stream user updated after onboarding for ${updatedUser.fullName}`,
+      );
+    } catch (streamError) {
+      console.log(
+        "Error updating Stream user during onboarding :",
+        streamError.message,
+      );
+    }
+    res.status(200).json({ success: true, user: updatedUser });
+  } catch (error) {
+    console.log("Onboarding Error", error);
+    res.status(500).json({ message: "Internal server Error" });
+  }
+}
