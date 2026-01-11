ed549e9f8d5cc0aa804eb2a815b5b72ebbebc38e
phantekzy <mainilotfi@gmail.com>
2026-01-11 18:40:14 +0100

    refactor: secure all user endpoints using router.use(protectRoute)

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -4,11 +4,12 @@ import { protectRoute } from "../middleware/auth.middleware.js";
 
 /* Router */
 const router = express.Router();
-
+// Apply auth middleware to all routes
+router.use(protectRoute);
 // Getting recommended users
-router.get("/", protectRoute, getrecommendedusers);
+router.get("/", getrecommendedusers);
 // Get Friends
-router.get("/friends", protectRoute, getMyFriends);
+router.get("/friends", getMyFriends);
 
 /* Export section */
 export default router;
