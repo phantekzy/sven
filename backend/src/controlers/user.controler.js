969eb4168866bca3d66880f466f526c1cd0b9faf
phantekzy <mainilotfi@gmail.com>
2026-01-12 20:17:26 +0100

    finished the recommended users logic

--- backend/src/controlers/user.controler.js
+++ backend/src/controlers/user.controler.js
@@ -1,5 +1,25 @@
+/* Import section */
+import User from "../models/User.js";
+
 /* Recommended users  section*/
-export async function getrecommendedusers(req, res) {}
+export async function getrecommendedusers(req, res) {
+  try {
+    const currentUserId = req.user.id;
+    const currentUser = req.user;
+
+    const recommendedUsers = await User.find({
+      $and: [
+        { _id: { $ne: currentUserId } },
+        { _id: { $nin: currentUser.friends } },
+        { isOnboarded: true },
+      ],
+    });
+    res.status(200).json(recommendedUsers);
+  } catch (error) {
+    console.error("Error in the getrecommendedusers controler", error.message);
+    res.status(500).json({ message: "Internal server Error" });
+  }
+}
 
 /* Friends section */
 export async function getMyFriends(req, res) {}
