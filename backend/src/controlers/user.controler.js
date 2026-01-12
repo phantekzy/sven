9caf3ce11fe1acf454cc1a4ffe73ad2826996ead
phantekzy <mainilotfi@gmail.com>
2026-01-12 20:29:19 +0100

    added the get friends section and added some improvements

--- backend/src/controlers/user.controler.js
+++ backend/src/controlers/user.controler.js
@@ -4,7 +4,7 @@ import User from "../models/User.js";
 /* Recommended users  section*/
 export async function getrecommendedusers(req, res) {
   try {
-    const currentUserId = req.user.id;
+    const currentUserId = req.user._id;
     const currentUser = req.user;
 
     const recommendedUsers = await User.find({
@@ -13,7 +13,7 @@ export async function getrecommendedusers(req, res) {
         { _id: { $nin: currentUser.friends } },
         { isOnboarded: true },
       ],
-    });
+    }).select("-password");
     res.status(200).json(recommendedUsers);
   } catch (error) {
     console.error("Error in the getrecommendedusers controler", error.message);
@@ -22,4 +22,20 @@ export async function getrecommendedusers(req, res) {
 }
 
 /* Friends section */
-export async function getMyFriends(req, res) {}
+export async function getMyFriends(req, res) {
+  try {
+    const user = await User.findById(req.user._id)
+      .select("friends")
+      .populate(
+        "friends",
+        "fullName profilePic nativeLanguage learningLanguage",
+      );
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json(user.friends);
+  } catch (error) {
+    console.error("Error in getMyFriends controler", error.message);
+    res.status(500).json({ message: "Internal server Error" });
+  }
+}
