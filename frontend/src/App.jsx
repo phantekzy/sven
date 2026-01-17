fdd4ebf943dec0d56e0a8b7cca87ed6f5041e79e
phantekzy <mainilotfi@gmail.com>
2026-01-17 02:30:47 +0100

    finished configuring the Routes in the app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,13 @@
 /* Import section */
 import { Route, Routes } from "react-router"
 // Routes 
-import HomePage from "./pages/HomePage"
-import SignUpPage from "./pages/SignUpPage"
+import HomePage from "./pages/HomePage.jsx"
+import SignUpPage from "./pages/SignUpPage.jsx"
+import LoginPage from "./pages/LoginPage.jsx"
+import NotificationsPage from "./pages/NotificationsPage.jsx"
+import CallPage from "./pages/CallPage.jsx"
+import ChatPage from "./pages/ChatPage.jsx"
+import OnboardingPage from "./pages/OnboardingPage.jsx"
 /* App component */
 const App = () => {
     return (
@@ -11,6 +16,11 @@ const App = () => {
                 <Routes>
                     <Route path="/" element={<HomePage />} />
                     <Route path="/signup" element={<SignUpPage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/notifications" element={<NotificationsPage />} />
+                    <Route path="/call" element={<CallPage />} />
+                    <Route path="/chat" element={<ChatPage />} />
+                    <Route path="/onboarding" element={<OnboardingPage />} />
                 </Routes>
             </div>
         </>
