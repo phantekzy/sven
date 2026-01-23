792c0f75182b6c1e5622108a2a7f5d886cb2454f
phantekzy <mainilotfi@gmail.com>
2026-01-23 03:05:36 +0100

    feat(onboarding): implement onboarding mutation with toast and cache invalidation

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -8,3 +8,5 @@ export const getAuthUser = async () => {
   const res = await axiosInstance.get("/auth/me");
   return res.data;
 };
+
+export const completeOnboarding = async () => {};
--- frontend/src/pages/OnboardingPage.jsx
+++ frontend/src/pages/OnboardingPage.jsx
@@ -1,8 +1,12 @@
 import { useState } from "react"
 import useAuthUser from "../hooks/useAuthUser"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { completeOnboarding } from "../lib/api"
+import toast from "react-hot-toast"
 
 const OnboardingPage = () => {
     const { authUser } = useAuthUser()
+    const queryClient = useQueryClient()
     const [formState, setFormState] = useState({
         fullName: authUser?.fullName || "",
         bio: authUser?.bio || "",
@@ -10,6 +14,13 @@ const OnboardingPage = () => {
         learningLanguage: authUser?.learningLanguage || "",
         location: authUser?.location || "",
         profilePic: authUser?.profilePic || "",
+    })
+    const { mutate: OnboardingMutation, isPending } = useMutation({
+        mutationFn: completeOnboarding,
+        onSuccess: () => {
+            toast.success("Sven Profile updated successfully !")
+            queryClient.invalidateQueries({ queryKey: ["authUser"] })
+        }
     })
     return (
         <div>Onboarding</div>
