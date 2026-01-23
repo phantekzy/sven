a20e2adead115ad131490d295102dc268953f6a3
phantekzy <mainilotfi@gmail.com>
2026-01-23 03:47:21 +0100

    feat(onboarding): build card layout and profile picture preview UI

--- frontend/src/pages/OnboardingPage.jsx
+++ frontend/src/pages/OnboardingPage.jsx
@@ -3,6 +3,7 @@ import useAuthUser from "../hooks/useAuthUser"
 import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { completeOnboarding } from "../lib/api"
 import toast from "react-hot-toast"
+import { CameraIcon } from "lucide-react"
 
 const OnboardingPage = () => {
     const { authUser } = useAuthUser()
@@ -27,7 +28,34 @@ const OnboardingPage = () => {
         OnboardingMutation(formState)
     }
     return (
-        <div>Onboarding</div>
+        <div className="min-h-screen bg-base-100 flex items-center justify-center p-4 ">
+            <div className="card bg-base-200 w-full max-w-3xl shadow-xl">
+                <div className="card-body p-6 sm:p-8">
+                    <h1 className="text-2xl sm:text-3xl font-bold text-center mb-6">Update your Profile</h1>
+                    <form
+                        onSubmit={handleSubmit}
+                        className="space-y-6"
+                    >
+                        {/* Profile picture section */}
+                        <div className="flex flex-col items-center justify-center space-y-4">
+                            <div className="size-32 rounded-full bg-base-300 overflow-hidden">
+                                {formState.profilePic ? (
+                                    <img
+                                        src={formState.profilePic}
+                                        alt="Sven profile picture"
+                                        className="w-full h-full object-cover"
+                                    />
+                                ) : (
+                                    <div className="flex items-center justify-center h-full">
+                                        <CameraIcon className="size-12 text-base-content opacity-40" />
+                                    </div>
+                                )}
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     )
 }
 export default OnboardingPage
