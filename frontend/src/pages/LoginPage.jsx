4e2363e8406e62c9d7f2167a3409c4877f458011
phantekzy <mainilotfi@gmail.com>
2026-01-26 18:11:47 +0100

    feat: design responsive login page layout with error handling and brand identity

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,6 +1,7 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { useState } from "react"
 import { login } from "../lib/api"
+import { ShipWheelIcon } from "lucide-react"
 
 /* Login page component */
 const LoginPage = () => {
@@ -18,7 +19,27 @@ const LoginPage = () => {
         loginMutation(loginData)
     }
     return (
-        <div>Login</div>
+        <div className="h-screen flex items-center justify-center p-4 sm:p-6 md:p-8 " data-theme="business">
+            <div className="border border-primary/25 flex flex-col lg:flex-row w-full max-w-5xl mx-auto bg-base-100 rounded-xl shadow-lg overflow-hidden">
+                {/* Login form section */}
+                <div className="w-full lg:w-1/2 p-4 sm:p-8 flex flex-col">
+                    <div className="mb-4 flex items-center justify-start gap-2">
+                        <ShipWheelIcon className="text-primary size-9" />
+                        <span className="text-3xl font-bold font-mono bg-clip-text text-transparent 
+                            bg-gradient-to-tr from-primary to-secondary tracking-wider">
+                            Sven
+                        </span>
+                    </div>
+
+                    {/* Error message display */}
+                    {error && (
+                        <div className="alert alert-error mb-4">
+                            <span>{error.response.data.message}</span>
+                        </div>
+                    )}
+                </div>
+            </div>
+        </div>
     )
 }
 /* Export section */
