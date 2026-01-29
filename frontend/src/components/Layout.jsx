bb93eaa14d060ccbcc91951598ad2aa12787c523
phantekzy <mainilotfi@gmail.com>
2026-01-30 00:40:17 +0100

    feat: implement nested flexbox layout with Navbar and Sidebar integration

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,10 +1,17 @@
+import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
 
-const Layout = ({showSidebar=false}) => {
+const Layout = ({ children ,showSidebar = false }) => {
   return (
     <div className="min-h-screen">
       <div className="flex">
         {showSidebar && <Sidebar />}
+        <div className="flex-1 flex flex-col">
+          <Navbar/>
+          <main>
+              {children}
+          </main>
+        </div>
       </div>
     </div>
   );
