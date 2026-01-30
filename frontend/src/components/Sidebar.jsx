fb703336192b8e372ce5b72ee01018e8a6a14785
phantekzy <mainilotfi@gmail.com>
2026-01-30 01:25:35 +0100

    build sidebar header with logo and sticky container

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,12 +1,30 @@
-import { useLocation } from "react-router";
+import { Link, useLocation } from "react-router";
 import useAuthUser from "../hooks/useAuthUser";
+import { ShipWheelIcon } from "lucide-react";
 
 const Sidebar = () => {
-  const{authUser}=useAuthUser()
-  const location = useLocation()
-  const currentPath = location.pathname
+  const { authUser } = useAuthUser();
+  const location = useLocation();
+  const currentPath = location.pathname;
 
-  return <div>Sidebar</div>;
+  return (
+    <aside
+      className="w-64 bg-base-200 border-r border-x-base-300 hidden lg:flex flex-col h-screen sticky
+  top-0"
+    >
+      <div className="p-5 border-b border-base-300">
+        <Link to="/" className="flex item gap-2.5">
+          <ShipWheelIcon className="size-9 text-primary" />
+          <span
+            className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-t 
+          from-primary to-secondary tracking-wider"
+          >
+            Sven
+          </span>
+        </Link>
+      </div>
+    </aside>
+  );
 };
 
 export default Sidebar;
