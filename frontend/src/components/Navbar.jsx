9236f479eb8f7637cae57b2c2731b40790683cf8
phantekzy <mainilotfi@gmail.com>
2026-01-30 18:23:59 +0100

    feat: add conditional branding to Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,8 @@
-import { useLocation } from "react-router";
+import { Link, useLocation } from "react-router";
 import useAuthUser from "../hooks/useAuthUser";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { logout } from "../lib/api";
+import { ShipWheelIcon } from "lucide-react";
 
 const Navbar = () => {
   const { authUser } = useAuthUser();
@@ -14,7 +15,25 @@ const Navbar = () => {
     onSuccess: () => queryClient.invalidateQueries({ queryKey: ["authUser"] }),
   });
 
-  return <div>Navbar</div>;
+  return (
+    <nav className="bg-base-200 border-b border-base-300 sticky top-0 z-30 h-16 flex items-center ">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-end w-full">
+          {/* Logo section Chat page only */}
+          {isChatPage && (
+            <div className="pl-5">
+              <Link to="/" className="flex items-center gap-2.5">
+                <ShipWheelIcon className="size-9 text-primary" />
+                <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-tr from-primary to-secondary tracking-wider">
+                  Sven
+                </span>
+              </Link>
+            </div>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
