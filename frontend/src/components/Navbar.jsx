1aa9babba848a526c770addc2978da41b0a476dd
phantekzy <mainilotfi@gmail.com>
2026-01-30 18:03:52 +0100

    im working on the logout mutation

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,18 @@
 import { useLocation } from "react-router";
 import useAuthUser from "../hooks/useAuthUser";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { logout } from "../lib/api";
 
 const Navbar = () => {
   const { authUser } = useAuthUser();
   const location = useLocation();
   const isChatPage = location.pathname?.startsWith("/chat");
+
+  const queryClient = useQueryClient();
+  useMutation({
+    mutationFn: logout,
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["authUser"] }),
+  });
 
   return <div>Navbar</div>;
 };
