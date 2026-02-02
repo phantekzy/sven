50d87f1c4c9ad4fea8f308c3ba852ba91ffd129a
phantekzy <mainilotfi@gmail.com>
2026-02-02 05:29:23 +0100

    i added  getOutgoingFriendReqs endpoint

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -38,3 +38,8 @@ export async function getRecommendedUsers() {
   const response = await axiosInstance.get("/users");
   return response.data;
 }
+
+export async function getOutgoingFriendReqs() {
+  const response = await axiosInstance.get("/users/outgoing-friend-requests");
+  return response.data;
+}
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,18 +1,29 @@
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
-import { getRecommendedUsers, getUserFriends } from "../lib/api";
+import {
+  getOutgoingFriendReqs,
+  getRecommendedUsers,
+  getUserFriends,
+} from "../lib/api";
 /* Home page component */
 const HomePage = () => {
   const queryClient = useQueryClient();
   const [outgoingRequestsids, setOutgoingRequestsIds] = useState([]);
 
+  // Friends
   const { data: friends = [], isLoading: loadingFriends } = useQuery({
     queryKey: ["friends"],
     queryFn: getUserFriends,
   });
+  // Recommended users
   const { data: recommendedUsers = [], isLoading: loadingUsers } = useQuery({
     queryKey: ["users"],
     queryFn: getRecommendedUsers,
+  });
+  // Outgoing friend Requests
+  const { data: outgoingFriendReqs } = useQuery({
+    queryKey: ["outgoingFriendReqs"],
+    queryFn: getOutgoingFriendReqs,
   });
 
   return <div>Home page</div>;
