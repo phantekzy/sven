13a21ebb8db4f9a9984569062a0860e758c201e4
phantekzy <mainilotfi@gmail.com>
2026-02-02 05:11:04 +0100

    feat: add useQuery hooks for friends and recommended users

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,19 @@
-import { useQueryClient } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
 /* Home page component */
 const HomePage = () => {
   const queryClient = useQueryClient();
   const [outgoingRequestsids, setOutgoingRequestsIds] = useState([]);
+
+  const { data: friends = [], isLoading: loadingFriends } = useQuery({
+    queryKey: ["friends"],
+    queryFn: getUserFriends,
+  });
+  const { data: recommendedUsers = [], isLoading: loadingUsers } = useQuery({
+    queryKey: ["users"],
+    queryFn: getRecommendedUsers,
+  });
+
   return <div>Home page</div>;
 };
 /* Export section */
