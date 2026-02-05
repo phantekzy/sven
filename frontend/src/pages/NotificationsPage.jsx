fe28f7fc24872c7d0ca77179bd84f39934d92957
phantekzy <mainilotfi@gmail.com>
2026-02-05 20:57:10 +0100

    I started working on the notification component

--- frontend/src/pages/NotificationsPage.jsx
+++ frontend/src/pages/NotificationsPage.jsx
@@ -1,5 +1,11 @@
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+
 /* Notifications page component */
 const NotificationsPage = () => {
+  const queryClient = useQueryClient()
+  const { data, isLoading } = useQuery({
+    queryKey: ["friendRequests"]
+  })
   return <div>Notifications</div>;
 };
 /* Export section */
